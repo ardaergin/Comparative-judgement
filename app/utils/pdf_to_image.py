c5c852624387ee70506b3f212ad844edef0aba03
ardaergin <ardergin@gmail.com>
2025-01-30 17:34:25 +0100

    Create pdf_to_image.py

---
+++ app/utils/pdf_to_image.py
@@ -0,0 +1,25 @@
+import fitz  # PyMuPDF
+
+def pdf_to_images(pdf_path, output_format="png", dpi=300):
+    doc = fitz.open(pdf_path)
+    zoom = dpi / 72  # Converting DPI to scaling factor (default PDF DPI is 72)
+    matrix = fitz.Matrix(zoom, zoom)
+    
+    for page_num in range(len(doc)):
+        page = doc.load_page(page_num)
+        pix = page.get_pixmap(matrix=matrix, alpha=False)  # Rendering page to image
+        
+        if page_num == 0:
+            output = f"texts/0_experiment_info.{output_format}"
+        if page_num == 1:
+            output = f"texts/4_practice_instructions.{output_format}"
+        if page_num == 2:
+            output = f"texts/5_trial_instructions_sim.{output_format}"
+        if page_num == 3:
+            output = f"texts/6_trial_instructions_liking.{output_format}"
+        pix.save(output)
+        print(f"Saved: {output}")
+    
+    doc.close()
+
+pdf_to_images("instructions.pdf", output_format="png", dpi=600)
