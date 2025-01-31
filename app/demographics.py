b81b77890c3f371a1b963648183f78c205f76b1f
B-Veckalov <106079220+B-Veckalov@users.noreply.github.com>
2025-01-31 21:38:28 +0100

    Update demographics.py

--- app/demographics.py
+++ app/demographics.py
@@ -28,15 +28,15 @@ def collect_demographics(win, participant_id):
         except ValueError:
             return False
 
-    age_question = "What is your age?(Please enter a number between 16 and 99)"
+    age_question = "What is your age?\n(Please enter a number between 16 and 99 and press ENTER)"
     demographics['age'] = free_text_prompt(win, age_question, validation_func=validate_age)
 
     # Nationality
-    nationality_question = "What is your nationality?"
+    nationality_question = "What is your nationality?\n (Please enter your nationality and press ENTER)\n "
     demographics['nationality'] = free_text_prompt(win, nationality_question, validation_func=None)
 
     # Dietary preference
-    dietary_question = "How would you describe your diet?\n(Please press the number on the left keyboard corresponding your chosen option. Do not use the keyboard on the right.)"
+    dietary_question = "How would you describe your diet?\n(Please press the number on the left keyboard corresponding your chosen option. Do not use the keyboard on the right.)\n"
     dietary_options = [
         "Omnivore (I eat animal products)",
         "Vegetarian",
@@ -47,13 +47,13 @@ def collect_demographics(win, participant_id):
     demographics['diet'] = multiple_choice_prompt(win, dietary_question, dietary_options)
 
     # Frequency of plant-based meat alternative eating
-    eat_frequency_question = "How often do you eat plant-based meat alternatives?\n(Please press the number on the left keyboard corresponding to your chosen option. Do not use the keyboard on the right.)"
+    eat_frequency_question = "How often do you eat plant-based meat alternatives?\n(Please press the number on the left keyboard corresponding to your chosen option. Do not use the keyboard on the right.)\n "
     eat_frequency_options = [
         "Never",
         "Once every three months",
         "Once a month",
         "Once a week",
-	"A couple of times per week",
+	    "A couple of times per week",
         "Every day"
     ]
     demographics['eat_frequency'] = multiple_choice_prompt(win, eat_frequency_question, eat_frequency_options)
