8d031fc31e2fbef2a71ad15b207926eb4ba89e97
B-Veckalov <106079220+B-Veckalov@users.noreply.github.com>
2025-01-31 16:33:36 +0100

    demographics_instructions_update

--- app/demographics.py
+++ app/demographics.py
@@ -16,7 +16,8 @@ def collect_demographics(win, participant_id):
     demographics = {}
 
     # Gender
-    gender_question = "What is your gender?"
+    gender_question = "What is your gender? 
+(Please press the number on the left keyboard corresponding to your chosen options. Do not use the keyboard on the right.)"
     gender_options = ["Male", "Female", "Non-binary", "Prefer not to say"]
     demographics['gender'] = multiple_choice_prompt(win, gender_question, gender_options)
 
@@ -36,7 +37,8 @@ def collect_demographics(win, participant_id):
     demographics['nationality'] = free_text_prompt(win, nationality_question, validation_func=None)
 
     # Dietary preference
-    dietary_question = "How would you describe your diet?"
+    dietary_question = "How would you describe your diet? 
+(Please press the number on the left keyboard corresponding your chosen option. Do not use the keyboard on the right.)"
     dietary_options = [
         "Omnivore (I eat animal products)",
         "Vegetarian",
@@ -46,27 +48,28 @@ def collect_demographics(win, participant_id):
     ]
     demographics['diet'] = multiple_choice_prompt(win, dietary_question, dietary_options)
 
-    # Frequency of plant-based meat alternative purchase
-    purchase_frequency_question = ("How often do you purchase plant-based meat alternatives?")
-    purchase_frequency_options = [
-        "Less than once a month",
-        "At least once a month, but less than once a week",
-        "Once or twice a week",
-        "More than twice a week, but less than everyday",
-        "At least once a day"
+    # Frequency of plant-based meat alternative eating
+    eat_frequency_question = "How often do you eat plant-based meat alternatives? (Please press the number on the left keyboard corresponding to your chosen option. Do not use the keyboard on the right.)"
+    eat_frequency_options = [
+        "Never",
+        "Once every three months",
+        "Once a month",
+        "Once a week",
+	"A couple of times per week",
+        "Every day"
     ]
-    demographics['purchase_frequency'] = multiple_choice_prompt(win, purchase_frequency_question, purchase_frequency_options)
+    demographics['eat_frequency'] = multiple_choice_prompt(win, eat_frequency_question, eat_frequency_options)
 
     # Save Demographic Data
     with open(f"data/{participant_id}_demographics.csv", 'w', newline='') as demo_file:
         demo_writer = csv.writer(demo_file)
-        demo_writer.writerow(['Gender', 'Age', 'Nationality', 'Diet', 'Purchase Frequency'])
+        demo_writer.writerow(['Gender', 'Age', 'Nationality', 'Diet', 'Eat Frequency'])
         demo_writer.writerow([
             demographics['gender'],
             demographics['age'],
             demographics['nationality'],
             demographics['diet'],
-            demographics['purchase_frequency']
+            demographics['eat_frequency']
         ])
 
     return demographics
