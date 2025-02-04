e42116498911636c697c2b34eb245e7c09e49283
ardaergin <ardergin@gmail.com>
2025-02-04 11:04:04 +0100

    Update instruction_images.py

--- app/instruction_images.py
+++ app/instruction_images.py
@@ -9,9 +9,9 @@ def create_scaled_image(win, image_path):
     )
     
     # Get the image's aspect ratio
-    aspect_ratio = img.size[0] / img.size[1]
+    # aspect_ratio = img.size[0] / img.size[1]
     
-    # Set height to 90% of window height and scale width accordingly
-    img.size = (2 * aspect_ratio, 2)  # Adjust 1.8 to change overall size
+    # # Set height to 90% of window height and scale width accordingly
+    # img.size = (2 * aspect_ratio, 2)  # Adjust 1.8 to change overall size
     
     return img
