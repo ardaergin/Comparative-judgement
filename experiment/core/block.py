5ab47ee57c54d64f024fb5096cf5fea823094028
themohammadhamdan <132884849+themohammadhamdan@users.noreply.github.com>
2025-04-22 17:23:38 +0200

    new coordinates test8

--- experiment/core/block.py
+++ experiment/core/block.py
@@ -84,14 +84,14 @@ class Block:
                 return {
                     # Comparison positions:
                     'left_image': (-0.3, -0.30),
-                    'left_text': (-0.3, -0.20),
+                    'left_text': (-0.3, -0.15),
 
                     'right_image': (0.3, -0.30),
-                    'right_text': (0.3, -0.20),
+                    'right_text': (0.3, -0.15),
 
                     # Reference positions:
-                    'reference_text': (0, 0.30),
-                    'reference_image': (0, 0)
+                    'reference_text': (0, 0.25),
+                    'reference_image': (0, 0.10)
                 }
             else: # if round_type == "pracice":
                 return {
